817f044e61101e48f78da2f28e039e21b0148901
Nurettin Kaya <sibbngheid@gmail.com>
2021-10-21 05:03:19 -0700

    lint

--- src/exports.ts
+++ src/exports.ts
@@ -69,7 +69,7 @@ const resolver = enhancedResolve.create.sync({
     '.json',
   ],
   modules: ['node_modules'],
-  conditionNames: ["import", "require"],
+  conditionNames: ['import', 'require'],
   mainFields: ['module', 'main'],
 })
 
